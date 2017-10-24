a5217432bbfcd1253ba69082a1a2a290f7826a0d
ScalpelApp <33061153+ScalpelApp@users.noreply.github.com>
2017-10-24 14:52:28 +0100

    Commit

---
+++ Scalpel-Alpha-0.0.1/Assets/Scripts/TrackedObjectsManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Vuforia;
+
+public class TrackedObjectsManager : MonoBehaviour {
+
+    private StateManager sm;
+
+    public Text scalpelText;
+    
+	// Use this for initialization
+	void Start () {
+        
+        //scalpelToggle.isOn = false;
+
+        //scalpelToggle.Text = "Not Tracked";
+
+        //scalpelEventHandler = scalpelImage.GetComponent<customEventHandler>().IsTrackedBool;
+
+        sm = TrackerManager.Instance.GetStateManager();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        IList<TrackableBehaviour> activeTrackables = (IList<TrackableBehaviour>)sm.GetActiveTrackableBehaviours();
+
+        foreach (TrackableBehaviour tb in activeTrackables)
+        {
+            // As you iterate, you compare with current game object name
+            // if you have 3 and 5 and target object is 5 then you get a match.
+            //scalpelToggle.Text = tb.TrackableName;
+
+            scalpelText.text = tb.TrackableName;
+        }
+
+        //if (GameObject.Find("ScalpelImageTarget").GetComponent<customEventHandler>().isTracked)
+        //{
+        //    print("***** IS TRACKED *****");
+
+        //    scalpelToggle.isOn = true;
+        //}
+    }
+}
---
+++ Scalpel-Alpha-0.0.1/Assets/Scripts/customEventHandler.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Vuforia
+{
+    /// <summary>
+    /// A custom handler that implements the ITrackableEventHandler interface.
+    /// </summary>
+    public class customEventHandler : MonoBehaviour,
+                                                ITrackableEventHandler
+    {
+        #region PRIVATE_MEMBER_VARIABLES
+
+        private TrackableBehaviour mTrackableBehaviour;
+        private bool isTracked = false;
+        private GameObject mScalpel;
+
+        #endregion // PRIVATE_MEMBER_VARIABLES
+
+        #region UNTIY_MONOBEHAVIOUR_METHODS
+
+        void Start()
+        {
+            mScalpel = GameObject.Find("Scalpel");
+
+            mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+            if (mTrackableBehaviour)
+            {
+                mTrackableBehaviour.RegisterTrackableEventHandler(this);
+            }
+        }
+
+        #endregion // UNTIY_MONOBEHAVIOUR_METHODS
+        
+        void Update()
+        {
+            if ( isTracked ) {
+                mScalpel.transform.Rotate(Vector3.up * 15 * Time.deltaTime);
+            }
+        }
+
+        #region PUBLIC_METHODS
+
+        /// <summary>
+        /// Implementation of the ITrackableEventHandler function called when the
+        /// tracking state changes.
+        /// </summary>
+        public void OnTrackableStateChanged(
+                                        TrackableBehaviour.Status previousStatus,
+                                        TrackableBehaviour.Status newStatus)
+        {
+            if (newStatus == TrackableBehaviour.Status.DETECTED ||
+                newStatus == TrackableBehaviour.Status.TRACKED ||
+                newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+            {
+                OnTrackingFound();
+            }
+            else
+            {
+                OnTrackingLost();
+            }
+        }
+        
+        #endregion // PUBLIC_METHODS
+
+
+
+        #region PRIVATE_METHODS
+
+
+        private void OnTrackingFound()
+        {
+            Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
+            Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
+
+            // Enable rendering:
+            foreach (Renderer component in rendererComponents)
+            {
+                component.enabled = true;
+            }
+
+            // Enable colliders:
+            foreach (Collider component in colliderComponents)
+            {
+                component.enabled = true;
+            }
+
+            IsTrackedBool = true;
+             
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " found");
+        }
+
+
+        private void OnTrackingLost()
+        {
+            Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
+            Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
+
+            // Disable rendering:
+            foreach (Renderer component in rendererComponents)
+            {
+                component.enabled = false;
+            }
+
+            // Disable colliders:
+            foreach (Collider component in colliderComponents)
+            {
+                component.enabled = false;
+            }
+
+            IsTrackedBool = false;
+
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
+        }
+
+        #endregion // PRIVATE_METHODS
+
+        public bool IsTrackedBool
+        {
+            get{ return this.isTracked; }
+            set
+            {
+                this.isTracked = value;
+            }
+        }
+    }
+}
